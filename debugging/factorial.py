6dde6d1d46addc8ca00a8ae5915a1966bf8f556d
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2024-05-27 16:35:04 +0200

    chore: Refactor factorial script to improve readability and remove unnecessary code

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers")
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n = n - 1
     return result
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <number>")
-        sys.exit(1)
-    
-    try:
-        num = int(sys.argv[1])
-        f = factorial(num)
-        print(f)
-    except ValueError as e:
-        print(e)
-        sys.exit(1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
